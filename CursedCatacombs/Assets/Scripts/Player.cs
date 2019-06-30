870572a602bb6b08358a5fc48acfaff12c46d212
TheNillesen <thenillesen@live.com>
2019-06-30 16:32:59 +0200

    hello

---
+++ CursedCatacombs/Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float rotationSpeed;
+
+    [SerializeField]
+    private Joystick joystickMove;
+    [SerializeField]
+    private Joystick joystickRotate;
+
+    private Quaternion targetModelRotation;
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+    }
+    private void GetInput()
+    {
+        //movement with the left joystick pad
+        float vertical = joystickMove.Vertical * speed * Time.deltaTime;
+        float horizontal = joystickMove.Horizontal * speed * Time.deltaTime;
+        
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, Vector2.SignedAngle(Vector2.up, new Vector3( joystickRotate.Horizontal,  joystickRotate.Vertical, 0))));
+        
+        transform.Translate(horizontal,vertical, 0,Space.World);
+    }
+    
+
+
+}
